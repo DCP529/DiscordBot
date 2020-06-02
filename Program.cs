058fd70db781b27e48ac1f95678f0f4a7f117239
DCP529 <61003286+DCP529@users.noreply.github.com>
2020-06-02 19:58:31 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,46 @@ namespace DiscordBot
     {
         static void Main(string[] args)
         {
+            
+
+            DiscordClient client;
+            client = new DiscordClient(new DiscordConfiguration()
+            {
+                Token =
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
+            });
+
+            client.MessageCreated += Client_MessageCreated;
+
+            client.ConnectAsync();
+
+            Console.ReadLine();
+        }
+
+        private async static Task Client_MessageCreated(DSharpPlus.EventArgs.MessageCreateEventArgs e)
+        {
+            var aut = e.Author.Username;
+            var msg = e.Message.Content;
+
+            Console.WriteLine($"{aut} {msg}");
+
+            if (msg.StartsWith("&"))
+            {
+                string res = string.Empty;
+                
+                switch (msg)
+                {
+                    case "&time": res = DateTime.Now.ToLongTimeString(); break;
+                    case "&Богорош": res = "Лох!"; break;
+                }
+
+
+                await e.Message.RespondAsync(res);
+            }
+            
         }
     }
 }
